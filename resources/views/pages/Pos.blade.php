d8980423429d785aa28baf2d46f3ff45d88e917b
josselrempis02 <josselrempis02@gmail.com>
2024-12-29 22:16:10 +0800

    adding/ui/payment

--- resources/views/pages/Pos.blade.php
+++ resources/views/pages/Pos.blade.php
@@ -95,25 +95,9 @@
 
                     <!-- Trigger Button -->
                     <button class="btn confirm-btn w-100" data-bs-toggle="modal" data-bs-target="#confirmModal">Confirm</button>
+                      <!-- Modal -->
+                      <x-payment-confirm/>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="confirmModalLabel">Confirmation</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            Are you sure you want to proceed?
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btn-primary">Confirm</button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
 
                 </div>
             </div>
